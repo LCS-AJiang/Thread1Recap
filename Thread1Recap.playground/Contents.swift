81aeed626ce0fe4be89bd29584eb0564512703a7
Audrey Jiang <ajiang@lcs.on.ca>
2023-01-10 08:58:35 -0500

    Initial Commit

---
+++ Thread1Recap.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Cocoa
+
+var age = 16
+let accelerationDueToGravity = 9.8
+struct Dog {
+    let breed: String
+    let color: String
+    let heightInMetres: Double
+}
+
+var dogAtLeft = Dog(breed: "English Bulldog",
+                     color: "Light brown, some white.",
+                     heightInMetres: 0.457)
+
+ var dogInMiddle = Dog(breed: "German Shepherd",
+                       color: "Black and dark brown.",
+                       heightInMetres: 0.753)
